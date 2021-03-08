5513668100ee7432eb83741e04e8bb0e19c20ef3
Khadeve <hoquangkha95@gmail.com>
2021-03-08 19:13:08 +0700

    update debug and exception tasks

--- src/_14_java_exception_and_debug/practice/_2_array_index_out_of_bound_exception/ArrayIndexOutOfBoundsExceptionTest.java
+++ src/_14_java_exception_and_debug/practice/_2_array_index_out_of_bound_exception/ArrayIndexOutOfBoundsExceptionTest.java
@@ -1,4 +1,33 @@
 package _14_java_exception_and_debug.practice._2_array_index_out_of_bound_exception;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayIndexOutOfBoundsExceptionTest {
+    public static void main(String[] args) {
+        Integer[] arr = createRandomArray();
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an index: ");
+        int index = in.nextInt();
+
+        try {
+            System.out.println("The element at index " + index + " is " + arr[index]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("The index is out of bounds.");
+        }
+
+    }
+
+    public static Integer[] createRandomArray() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("List of elements in array:");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        return arr;
+    }
 }
--- src/_14_java_exception_and_debug/practice/_3_number_format_exception/Calculator.java
+++ src/_14_java_exception_and_debug/practice/_3_number_format_exception/Calculator.java
@@ -1,4 +1,27 @@
 package _14_java_exception_and_debug.practice._3_number_format_exception;
 
+import java.util.*;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first operand: ");
+        int firstOperand = in.nextInt();
+        System.out.print("Enter the second operand: ");
+        int secondOperand = in.nextInt();
+
+        calculate(firstOperand, secondOperand);
+    }
+
+    public static void calculate(int x, int y) {
+        try {
+            System.out.println("Summary: " + (x + y));
+            System.out.println("Difference: " + (x - y));
+            System.out.println("Product: " + (x * y));
+            System.out.println("Quotient: " + (x / y));
+        } catch(Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
+        }
+    }
 }
