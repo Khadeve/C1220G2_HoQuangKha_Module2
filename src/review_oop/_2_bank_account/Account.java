097a22bb98893d87014bf7b1f84965bfc49a4267
Khadeve <hoquangkha95@gmail.com>
2021-03-08 19:35:34 +0700

    update review_oop

--- src/review_oop/_2_bank_account/Account.java
+++ src/review_oop/_2_bank_account/Account.java
@@ -1,4 +1,106 @@
 package review_oop._2_bank_account;
 
+import java.util.*;
+
 public class Account {
+    private static final double INTEREST = 0.035;
+    private static final double WITHDRAWAL_FEE = 1.5;
+
+    private long accountNumber;
+    private String accountName;
+    private double balance;
+
+    public Account(){}
+
+    public Account(long accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        balance = 50;
+    }
+
+    public Account(long accountNumber, String accountName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber=" + accountNumber +
+                ", accountName='" + accountName + '\'' +
+                ", balance=" + balance + "$" +
+                '}';
+    }
+
+    private double enterMoney() {
+        Scanner in = new Scanner(System.in);
+        double money;
+
+        do {
+            System.out.print("Enter the amount of money: ");
+            money = in.nextDouble();
+            if (money < 0 || money > balance) {
+                System.out.println("Invalid amount. Please try again!");
+            } else {
+                break;
+            }
+        } while (true);
+
+        return money;
+    }
+
+    public void payIn() {
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Enter the amount of money: ");
+            double money = in.nextDouble();
+            if (money < 0) {
+                System.out.println("The amount is negative. Please try again!");
+            } else {
+                balance += money;
+                break;
+            }
+        } while (true);
+    }
+
+    public void withdraw() {
+        double money = enterMoney();
+        balance -= (money + WITHDRAWAL_FEE);
+    }
+
+    public double mature() {
+        balance += (balance * INTEREST);
+        return balance;
+    }
+
+    public void transferMoney(Account other) {
+        double money = enterMoney();
+        balance -= money;
+        other.balance += money;
+    }
 }
--- src/review_oop/_2_bank_account/AccountTest.java
+++ src/review_oop/_2_bank_account/AccountTest.java
@@ -1,4 +1,142 @@
 package review_oop._2_bank_account;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AccountTest {
+    private static final String MAIN_MENU = "\n---------------------Main Menu---------------------" +
+            "\n1. Add New Account." +
+            "\n2. Show Accounts." +
+            "\n3. Process on Account." +
+            "\n4. Exit." +
+            "\n---------------------------------------------------";
+
+    private static final String ACCOUNT_MENU = "\n---------------------Account Menu---------------------" +
+            "\n1. Pay in." +
+            "\n2. With draw." +
+            "\n3. Transfer money." +
+            "\n4. Show information." +
+            "\n5. Mature." +
+            "\n6. Back to main menu." +
+            "\n---------------------------------------------------";
+
+    public static void main(String[] args) {
+        ArrayList<Account> accountList = new ArrayList<>();
+
+        accountList.add(new Account(1111, "Ho Quang Kha"));
+        accountList.add(new Account(2222, "Do Viet Hoai"));
+
+        Scanner in = new Scanner(System.in);
+
+        int choice;
+        do {
+            System.out.println(MAIN_MENU);
+            System.out.print("***Please enter your choice: ");
+            choice = in.nextInt();
+            switch(choice) {
+                case 1:
+                    addNewAccount(accountList);
+                    break;
+                case 2:
+                    showAccounts(accountList);
+                    break;
+                case 3:
+                    Account chosenAccount = chooseAnAccount(accountList);
+
+                    // Create a copied list without the chosenAccount.
+                    ArrayList<Account> newList = new ArrayList<>();
+                    accountList.forEach((e) -> {
+                        if (e != chosenAccount) {
+                            newList.add(e);
+                        }
+                    });
+
+                    operateOnAccount(chosenAccount, newList);
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again!");
+            }
+        } while(choice != 4);
+    }
+
+    private static void addNewAccount(List<Account> list) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("1. Enter the account number: ");
+        long accountNumber = in.nextLong();
+
+        System.out.print("2. Enter the name of the account: ");
+        in.nextLine();  // Consume newline.
+        String accountName = in.nextLine();
+
+        list.add(new Account(accountNumber, accountName));
+    }
+
+    private static void showAccounts(List<Account> list) {
+        AtomicInteger counter = new AtomicInteger(1);
+        list.forEach((e) ->
+                System.out.println(counter.getAndIncrement() + "." + e));
+    }
+
+    private static Account chooseAnAccount(List<Account> list) {
+        showAccounts(list);
+
+        Scanner in = new Scanner(System.in);
+
+        int chooseAccount;
+        boolean flag;
+        do {
+            System.out.print("***Choose an account: ");
+            chooseAccount = in.nextInt();
+            flag = (chooseAccount <= 0 || chooseAccount > list.size());
+            if (flag) {
+                System.out.println("!Invalid input. Please try again.");
+            }
+        } while(flag);
+
+        return list.get(chooseAccount - 1);
+    }
+
+    private static void operateOnAccount(Account account, List<Account> list) {
+        System.out.println(ACCOUNT_MENU);
+
+        Scanner in = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.print("***Enter your choice: ");
+            choice = in.nextInt();
+
+            switch(choice) {
+                case 1:
+                    account.payIn();
+                    System.out.println("Pay in done");
+                    break;
+                case 2:
+                    account.withdraw();
+                    System.out.println("Withdrawing done");
+                    break;
+                case 3:
+                    System.out.println("***Choose a receiver account:");
+                    Account receiver = chooseAnAccount(list);
+                    account.transferMoney(receiver);
+                    System.out.println("Transferring done");
+                    break;
+                case 4:
+                    System.out.println(account);
+                    break;
+                case 5:
+                    account.mature();
+                    System.out.println("Maturing done");
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("!Invalid choice. Please try again.");
+            }
+        } while(choice != 6);
+
+    }
 }
